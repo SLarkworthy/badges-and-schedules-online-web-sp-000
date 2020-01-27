6d0340c4328ab5ad1df4eddcbb1976d241f46f12
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-27 20:17:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_array = []
+  array.each do |name|
+    badge_array << "Hello, my name is #{name}."
+  end
+  return badge_array
+end
+
+def assign_rooms(array)
+  room_array = []
+  array.each_with_index {|item, index|
+end
